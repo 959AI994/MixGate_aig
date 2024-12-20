3b5dfb69b52cf83a2b021c65ca95535826adeff8
wangjingxin <1114263143@qq.com>
2024-12-20 08:21:06 +0000

    feat: add code

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import deepcell
+import mixgate
 import torch
 import os
 from config import get_parse_args
@@ -16,12 +16,12 @@ if __name__ == '__main__':
     num_epochs = 60
     
     print('[INFO] Parse Dataset')
-    dataset = deepcell.NpzParser(DATA_DIR, circuit_path)
+    dataset = mixgate.NpzParser(DATA_DIR, circuit_path)
     train_dataset, val_dataset = dataset.get_dataset()
     print('[INFO] Create Model and Trainer')
-    model = deepcell.Model()
+    model = mixgate.Model()
     
-    trainer = deepcell.Trainer(args, model, distributed=True)
+    trainer = mixgate.Trainer(args, model, distributed=True)
     trainer.set_training_args(loss_weight=[3.0, 1.0, 0.5], lr=1e-4, lr_step=50)
     print('[INFO] Stage 1 Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
--- train_mask.py
+++ train_mask.py
@@ -2,12 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import deepcell
+import mixgate
 import torch
 import os
 from config import get_parse_args
-import deepcell.top_model
-import deepcell.top_trainer 
+import mixgate.top_model
+import mixgate.top_trainer 
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 DATA_DIR = './data/dg_pair'
@@ -18,16 +18,16 @@ if __name__ == '__main__':
     num_epochs = args.num_epochs
     
     print('[INFO] Parse Dataset')
-    dataset = deepcell.NpzParser_Pair(DATA_DIR, circuit_path)
+    dataset = mixgate.NpzParser_Pair(DATA_DIR, circuit_path)
     train_dataset, val_dataset = dataset.get_dataset()
     print('[INFO] Create Model and Trainer')
-    model = deepcell.top_model.TopModel(
+    model = mixgate.top_model.TopModel(
         args, 
         dc_ckpt='./ckpt/dc.pth', 
         dg_ckpt='./ckpt/dg.pth'
     )
     
-    trainer = deepcell.top_trainer.TopTrainer(args, model, distributed=True)
+    trainer = mixgate.top_trainer.TopTrainer(args, model, distributed=True)
     trainer.set_training_args(lr=1e-4, lr_step=50)
     print('[INFO] Stage 1 Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
