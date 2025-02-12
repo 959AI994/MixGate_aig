6fd6acd76fa774734545349820fa78364912992e
wangjingxin <1114263143@qq.com>
2025-02-12 11:02:02 +0000

    feat: update dg_model_xmg.py

--- mixgate/dg_model_xmg.py
+++ mixgate/dg_model_xmg.py
@@ -38,17 +38,26 @@ class Model(nn.Module):
         # Network 
         self.aggr_and_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
         self.aggr_not_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
+        self.aggr_xor_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
+        self.aggr_maj_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
+        self.aggr_or_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
+
         self.aggr_and_func = TFMlpAggr(self.dim_hidden*2, self.dim_hidden)
         self.aggr_not_func = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
-        self.aggr_xor_strc = TFMlpAggr(self.dim_hidden*1, self.dim_hidden)
         self.aggr_xor_func = TFMlpAggr(self.dim_hidden*2, self.dim_hidden)
-            
+        self.aggr_maj_func = TFMlpAggr(self.dim_hidden*2, self.dim_hidden)
+        self.aggr_or_func = TFMlpAggr(self.dim_hidden*2, self.dim_hidden)
+        
         self.update_and_strc = GRU(self.dim_hidden, self.dim_hidden)
         self.update_and_func = GRU(self.dim_hidden, self.dim_hidden)
         self.update_not_strc = GRU(self.dim_hidden, self.dim_hidden)
         self.update_not_func = GRU(self.dim_hidden, self.dim_hidden)
         self.update_xor_strc = GRU(self.dim_hidden, self.dim_hidden)
         self.update_xor_func = GRU(self.dim_hidden, self.dim_hidden)
+        self.update_maj_strc = GRU(self.dim_hidden, self.dim_hidden)
+        self.update_maj_func = GRU(self.dim_hidden, self.dim_hidden)
+        self.update_or_strc = GRU(self.dim_hidden, self.dim_hidden)
+        self.update_or_func = GRU(self.dim_hidden, self.dim_hidden)
 
         # Readout 
         self.readout_prob = MLP(self.dim_hidden, self.dim_mlp, 1, num_layer=3, p_drop=0.2, norm_layer='batchnorm', act_layer='relu')
@@ -82,34 +91,30 @@ class Model(nn.Module):
         edge_index = G.xmg_edge_index
 
         #print("G.gate =", G.gate)
+        # 获取每种门的掩码
         node_state = torch.cat([hs, hf], dim=-1)
-        and_mask = G.xmg_gate.squeeze(1) == 1 #判断门的种类
-        not_mask = G.xmg_gate.squeeze(1) == 2
-        xor_mask = G.xmg_gate.squeeze(1) == 3     
-        # print("xor_mask =", xor_mask)
-        
+        and_mask = G.gate.squeeze(1) == 1
+        not_mask = G.gate.squeeze(1) == 2
+        xor_mask = G.gate.squeeze(1) == 3
+        maj_mask = G.gate.squeeze(1) == 4
+        or_mask = G.gate.squeeze(1) == 5
 
         for _ in range(self.num_rounds):
             for level in range(1, num_layers_f):
-                # forward layer
-                layer_mask = G.xmg_forward_level == level #将目标层级不mask掉，G.forward_level是各节点的层级信息
-                # print("G.level =", G.forward_level)
-                # print("layer_mask =", layer_mask)
+                # 正向传播的层
+                layer_mask = G.forward_level == level  # 获取目标层级的mask
 
                 # AND Gate
-                l_and_node = G.xmg_forward_index[layer_mask & and_mask]#将目标层级的and节点显示出来
-                # print("l_and_node =", l_and_node)
+                l_and_node = G.forward_index[layer_mask & and_mask]
                 if l_and_node.size(0) > 0:
                     and_edge_index, and_edge_attr = subgraph(l_and_node, edge_index, dim=1)
-                    
-                    
-                    # Update structure hidden state
+                    # 更新结构隐藏状态
                     msg = self.aggr_and_strc(hs, and_edge_index, and_edge_attr)
                     and_msg = torch.index_select(msg, dim=0, index=l_and_node)
                     hs_and = torch.index_select(hs, dim=0, index=l_and_node)
                     _, hs_and = self.update_and_strc(and_msg.unsqueeze(0), hs_and.unsqueeze(0))
                     hs[l_and_node, :] = hs_and.squeeze(0)
-                    # Update function hidden state
+                    # 更新功能隐藏状态
                     msg = self.aggr_and_func(node_state, and_edge_index, and_edge_attr)
                     and_msg = torch.index_select(msg, dim=0, index=l_and_node)
                     hf_and = torch.index_select(hf, dim=0, index=l_and_node)
@@ -117,43 +122,74 @@ class Model(nn.Module):
                     hf[l_and_node, :] = hf_and.squeeze(0)
 
                 # NOT Gate
-                l_not_node = G.xmg_forward_index[layer_mask & not_mask]
+                l_not_node = G.forward_index[layer_mask & not_mask]
                 if l_not_node.size(0) > 0:
                     not_edge_index, not_edge_attr = subgraph(l_not_node, edge_index, dim=1)
-                    # Update structure hidden state
+                    # 更新结构隐藏状态
                     msg = self.aggr_not_strc(hs, not_edge_index, not_edge_attr)
                     not_msg = torch.index_select(msg, dim=0, index=l_not_node)
                     hs_not = torch.index_select(hs, dim=0, index=l_not_node)
                     _, hs_not = self.update_not_strc(not_msg.unsqueeze(0), hs_not.unsqueeze(0))
                     hs[l_not_node, :] = hs_not.squeeze(0)
-                    # Update function hidden state
+                    # 更新功能隐藏状态
                     msg = self.aggr_not_func(hf, not_edge_index, not_edge_attr)
                     not_msg = torch.index_select(msg, dim=0, index=l_not_node)
                     hf_not = torch.index_select(hf, dim=0, index=l_not_node)
                     _, hf_not = self.update_not_func(not_msg.unsqueeze(0), hf_not.unsqueeze(0))
                     hf[l_not_node, :] = hf_not.squeeze(0)
-                
-                l_xor_node = G.xmg_forward_index[layer_mask & xor_mask]#将目标层级的and节点显示出来
-                # print("l_and_node =", l_and_node)
+
+                # XOR Gate
+                l_xor_node = G.forward_index[layer_mask & xor_mask]
                 if l_xor_node.size(0) > 0:
                     xor_edge_index, xor_edge_attr = subgraph(l_xor_node, edge_index, dim=1)
-                    
-                    
-                    # Update structure hidden state
+                    # 更新结构隐藏状态
                     msg = self.aggr_xor_strc(hs, xor_edge_index, xor_edge_attr)
                     xor_msg = torch.index_select(msg, dim=0, index=l_xor_node)
                     hs_xor = torch.index_select(hs, dim=0, index=l_xor_node)
                     _, hs_xor = self.update_xor_strc(xor_msg.unsqueeze(0), hs_xor.unsqueeze(0))
                     hs[l_xor_node, :] = hs_xor.squeeze(0)
-                    # Update function hidden state
+                    # 更新功能隐藏状态
                     msg = self.aggr_xor_func(node_state, xor_edge_index, xor_edge_attr)
                     xor_msg = torch.index_select(msg, dim=0, index=l_xor_node)
-                    hs_xor = torch.index_select(hf, dim=0, index=l_xor_node)
-                    _, hs_xor = self.update_xor_func(xor_msg.unsqueeze(0), hs_xor.unsqueeze(0))
-                    hf[l_xor_node, :] = hs_xor.squeeze(0)
-                
-                
-                # Update node state
+                    hf_xor = torch.index_select(hf, dim=0, index=l_xor_node)
+                    _, hf_xor = self.update_xor_func(xor_msg.unsqueeze(0), hf_xor.unsqueeze(0))
+                    hf[l_xor_node, :] = hf_xor.squeeze(0)
+
+                # Majority Gate
+                l_maj_node = G.forward_index[layer_mask & maj_mask]
+                if l_maj_node.size(0) > 0:
+                    maj_edge_index, maj_edge_attr = subgraph(l_maj_node, edge_index, dim=1)
+                    # 更新结构隐藏状态
+                    msg = self.aggr_maj_strc(hs, maj_edge_index, maj_edge_attr)
+                    maj_msg = torch.index_select(msg, dim=0, index=l_maj_node)
+                    hs_maj = torch.index_select(hs, dim=0, index=l_maj_node)
+                    _, hs_maj = self.update_maj_strc(maj_msg.unsqueeze(0), hs_maj.unsqueeze(0))
+                    hs[l_maj_node, :] = hs_maj.squeeze(0)
+                    # 更新功能隐藏状态
+                    msg = self.aggr_maj_func(node_state, maj_edge_index, maj_edge_attr)
+                    maj_msg = torch.index_select(msg, dim=0, index=l_maj_node)
+                    hf_maj = torch.index_select(hf, dim=0, index=l_maj_node)
+                    _, hf_maj = self.update_maj_func(maj_msg.unsqueeze(0), hf_maj.unsqueeze(0))
+                    hf[l_maj_node, :] = hf_maj.squeeze(0)
+
+                # OR Gate
+                l_or_node = G.forward_index[layer_mask & or_mask]
+                if l_or_node.size(0) > 0:
+                    or_edge_index, or_edge_attr = subgraph(l_or_node, edge_index, dim=1)
+                    # 更新结构隐藏状态
+                    msg = self.aggr_or_strc(hs, or_edge_index, or_edge_attr)
+                    or_msg = torch.index_select(msg, dim=0, index=l_or_node)
+                    hs_or = torch.index_select(hs, dim=0, index=l_or_node)
+                    _, hs_or = self.update_or_strc(or_msg.unsqueeze(0), hs_or.unsqueeze(0))
+                    hs[l_or_node, :] = hs_or.squeeze(0)
+                    # 更新功能隐藏状态
+                    msg = self.aggr_or_func(node_state, or_edge_index, or_edge_attr)
+                    or_msg = torch.index_select(msg, dim=0, index=l_or_node)
+                    hf_or = torch.index_select(hf, dim=0, index=l_or_node)
+                    _, hf_or = self.update_or_func(or_msg.unsqueeze(0), hf_or.unsqueeze(0))
+                    hf[l_or_node, :] = hf_or.squeeze(0)
+
+                # 更新节点状态
                 node_state = torch.cat([hs, hf], dim=-1)
 
         node_embedding = node_state.squeeze(0)
