8d07e5bca0ebb2fda82b4497e05f47ff7f6b7207
JingxinWang <1114263143@qq.com>
2025-03-13 15:40:14 +0800

    feat: add mixgate aig pth file

--- train_mask.py
+++ train_mask.py
@@ -38,13 +38,18 @@ if __name__ == '__main__':
     )
 
     trainer = mixgate.top_trainer.TopTrainer(args, model, distributed=True)
-    trainer.set_training_args(lr=1e-4, lr_step=50, loss_weight = [1.0, 1.0, 0])
+    trainer.set_training_args(lr=1e-4, lr_step=50, loss_weight = [0.0, 1.0, 0])
     print('[INFO] Stage 1 Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
 
     print('[INFO] Stage 2 Training ...')
-    trainer.set_training_args(loss_weight = [3.0, 1.0, 2.0], lr=1e-4, lr_step=50)
+    trainer.set_training_args(loss_weight = [1.0, 0.0, 0.0], lr=1e-4, lr_step=50)
     trainer.train(num_epochs, train_dataset, val_dataset)
+
+    print('[INFO] Stage 3 Training ...')
+    trainer.set_training_args(loss_weight = [0.0, 0.0, 1.0], lr=1e-4, lr_step=50)
+    trainer.train(num_epochs, train_dataset, val_dataset)
+
 
     
     
